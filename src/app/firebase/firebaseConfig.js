44d90bc8ee32922f7cd78d4d654a87c271646d4b
suleman098 <81332154+suleman098@users.noreply.github.com>
2025-01-08 21:57:00 +0000

    Update firebaseConfig.js

--- src/app/firebase/firebaseConfig.js
+++ src/app/firebase/firebaseConfig.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAJcsxEp8uZxlRALN3-yXnvce05C0GuKVE",
-  authDomain: "infohub-83b45.firebaseapp.com",
-  projectId: "infohub-83b45",
-  storageBucket: "infohub-83b45.appspot.com",
-  messagingSenderId: "569335732733",
-  appId: "1:569335732733:web:01a815f09f1616fa396194",
-  measurementId: "G-H962J454NT",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
